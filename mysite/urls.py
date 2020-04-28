9474b4308c6fef9f4832f3936a79658bb2a44655
anastasiiakhil <staceymakfin@gmail.com>
2020-04-28 22:26:44 +0300

    add authorizing

--- mysite/urls.py
+++ mysite/urls.py
@@ -1,7 +1,10 @@
 from django.conf.urls import url, include
 from django.contrib import admin
+from django.contrib.auth import views
 
 urlpatterns = [
     url(r'^admin/', admin.site.urls),
+    url(r'/login/', views.LoginView.as_view(), name='login'),
+    url(r'/logout/', views.LogoutView.as_view(next_page='/'), name='logout'),
     url(r'', include('blog.urls')),
 ]
