db69c768ecfee8deb6f7f22fa8d14de32893e61c
anastasiiakhil <staceymakfin@gmail.com>
2020-04-30 11:56:30 +0300

    fix authentication

--- mysite/urls.py
+++ mysite/urls.py
@@ -3,8 +3,8 @@ from django.contrib import admin
 from django.contrib.auth import views
 
 urlpatterns = [
-    url(r'^admin/', admin.site.urls),
-    url(r'/login/', views.LoginView.as_view(), name='login'),
-    url(r'/logout/', views.LogoutView.as_view(next_page='/'), name='logout'),
+    url(r'admin/', admin.site.urls),
+    url(r'accounts/login/', views.LoginView.as_view(), name='login'),
+    url(r'accounts/logout/', views.LogoutView.as_view(next_page='/'), name='logout'),
     url(r'', include('blog.urls')),
 ]
